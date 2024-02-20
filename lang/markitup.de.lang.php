f3d182515ce0b5067dba0183cb75a5940b4d1713
Alexey Kalnov <kalnovalexey@yandex.ru>
2024-02-20 10:08:27 +0300

    Add lang files

---
+++ lang/markitup.de.lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * German Language File for MarkItUp Plugin
+ *
+ * @package markitup
+ * @version 0.7.0
+ * @author Cotonti Translators Team
+ * @copyright Copyright (c) Cotonti Team 2008-2011
+ * @license BSD
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+/**
+ * Plugin Config
+ */
+
+$L['cfg_autorefresh'] = array('Automatische Aktualisierung der Vorschau');
+$L['cfg_chili'] = array('Chili-Tags einschalten');
+$L['cfg_skin'] = array('Skin (plugins/markitup/skins/SKIN_NAME)');
+
+$L['info_desc'] = 'BBcode/HTML-Inhaltseditor auf jQuery-Basis. Es sollte nur ein Editor verwendet werden, da es sonst zu Konflikten führen kann.';
+
+?>
